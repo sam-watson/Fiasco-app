efc2bdbfdcc894e890a1567675376d11e8686404
simoom22 <swatson4@kent.edu>
2013-11-19 16:47:05 -0500

    removing old json impl code

--- Assets/Playsets/Playset.cs
+++ Assets/Playsets/Playset.cs
@@ -19,39 +19,6 @@ public class Playset {
 		this.thumbImage = Resources.Load(thumbImg) as Texture2D;
 		this.elements = elements;
 	}
-	
-//	// JSON to class conversion
-//	public static explicit operator Playset(JSON value) {
-//		checked {
-//			return new Playset(
-//				value.ToString("name"),
-//				value.ToString("summary"),
-//				value.ToString("coverImage"),
-//				value.ToString("thumbImage"),
-//				(PlaysetElements)value.ToJSON("elements"));
-//		}
-//	}
-//	
-//	// convert a JSON array to a class Array
-//	public static Playset[] Array(JSON[] array) {
-//		List<Playset> tc = new List<Playset>();
-//		for (int i=0; i<array.Length; i++) {
-//			tc.Add((Playset)array[i]);
-//		}
-//		return tc.ToArray();
-//	}
-//	
-//	//serializer - not sure if needed, done for practice
-//	public static implicit operator JSON(Playset value) {
-//		JSON js = new JSON();
-//		js["name"] = value.name;
-//		js["summary"] = value.summary;
-//		js["coverImage"] = value.coverImage.name;
-//		js["thumbImage"] = value.thumbImage.name;
-//		JSON jsElements = (JSON)value.elements;
-//		js["elements"] = jsElements;
-//		return js;
-//	}
 }
 
 public class PlaysetElements {
@@ -66,25 +33,4 @@ public class PlaysetElements {
 		this.relationships = relationships;
 	}
 	
-	// JSON to class conversion
-	public static explicit operator PlaysetElements(JSON value) {
-		checked {
-			return new PlaysetElements(
-				UnpackJsonElements(value, "relationships"),
-				UnpackJsonElements(value, "needs"),
-				UnpackJsonElements(value, "locations"),
-				UnpackJsonElements(value, "objects"));
-		}
-	}
-	
-	private static Dictionary<string, List<string>> UnpackJsonElements(JSON json, string name) {
-		JSON jsElement = json.ToJSON(name);
-		var dict = new Dictionary<string, List<string>>();
-		foreach (var e in jsElement.fields) {
-			if (e.Value is IEnumerable) {
-				dict.Add(e.Key, e.Value as List<string>);
-			}
-		}
-		return dict;
-	}
 }
