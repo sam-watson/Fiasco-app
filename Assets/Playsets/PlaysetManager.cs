85d600e64baae3ef59d1068bf0b11836eaa8a5a8
simoom22 <swatson4@kent.edu>
2013-12-31 15:43:48 -0500

    playset elements parsing

--- Assets/Playsets/PlaysetManager.cs
+++ Assets/Playsets/PlaysetManager.cs
@@ -20,11 +20,65 @@ public class PlaysetManager : MonoBehaviour {
 			var playsetName = playsetData["playsets"][i].ToString();
 			var playset = new Playset();
 			playset.name = playsetName;
-			var infoText = (TextAsset)Resources.Load(playsetName + "/" + playsetName);
+			var infoText = (TextAsset)Resources.Load(playsetName + "/" + playsetName + " Info");
 			playset.info = JsonMapper.ToObject<PlaysetInfo>(infoText.text);
+			var elementText = (TextAsset)Resources.Load(playsetName + "/" + playsetName + " Elements");
+			playset.elements = ParseElements(elementText.text);
 			//TODO: images
 			playsets.Add(playset);
 			Debug.Log("Loading... " + playsets.IndexOf(playset) + ": " + playset.name);
 		}
+	}
+	
+	public PlaysetElements ParseElements(string bunchoText) {
+		var elements = new PlaysetElements();
+		int c = 0;
+		elements.relationships = ElementParser(bunchoText, ref c);
+		elements.needs = ElementParser(bunchoText, ref c);
+		elements.locations = ElementParser(bunchoText, ref c);
+		elements.objects = ElementParser(bunchoText, ref c);
+		return elements;
+	}
+	
+	private Dictionary<string, List<string>> ElementParser(string readStr, ref int c) {
+		//iterate through big string looking for numbers and new-lines, 6 sets of 6
+		var element = new Dictionary<string, List<string>>();
+		for (int i=1; i<=6; i++) {
+			string keyStr;
+			keyStr = SubElementParser(readStr, ref c, i);
+			var subElems = new List<string>();
+			for (int ii=1; ii<=6; ii++) {
+				subElems.Add(SubElementParser(readStr, ref c, ii));
+			}
+			element.Add(keyStr, subElems);
+		}
+		//verify
+		bool complete = true;
+		if (element.Count != 6) {complete = false;}
+		foreach (var subElem in element.Values) {
+			if (subElem.Count != 6) {complete = false;}
+		}
+		if (complete == false) {Debug.Log("*** problem parsing playset elements!!! ***");}
+		
+		return element;
+	}
+	
+	private string SubElementParser(string readStr, ref int c, int i) {
+		string buildStr = "";
+		if ( readStr[c].ToString() == i.ToString() && readStr[c+1].ToString() == " ") {
+			c = c+2;
+			//Debug.Log("----number found");
+		}
+		while ( c < readStr.Length ) {
+			if ( readStr[c] == (char)13 && readStr[c+1] == (char)10 ) {
+				//Debug.Log("----escape found");
+				c = c+2;
+				break;
+			} else {
+				buildStr += readStr[c].ToString();
+				c++;
+			}
+		}
+		return buildStr;
 	}
 }
